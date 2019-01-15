b27f3a01511ecd37b1661ecf99a95a423dc01f15
lucasquieto <41813276+lucasquieto@users.noreply.github.com>
2019-01-15 16:39:19 -0200

    estudo c#

---
+++ estudoteste/Assets/Scripts/Exemplo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exemplo : MonoBehaviour {
+
+    private string nome = "Lucas";
+    private int idade = 10;
+    private bool obesidade = true;
+    private float altura = 1.80f;
+
+	// Use this for initialization
+	void Start () {
+
+        Debug.Log(nome);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
